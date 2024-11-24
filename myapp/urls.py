8439eb0e71e5ce82245156c359eacb3927be9b4e
monir3430 <miamonir3430@gmail.com>
2024-11-24 23:03:37 +0600

    login

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('success/', views.success_view, name='success_url'),
+    path('login/', views.login_view, name='login'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -23,3 +25,20 @@ def signup(request):
 
 def success_view(request):
     return render(request, 'success.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            # Log in the user
+            user = form.get_user()
+            login(request, user)
+            # Redirect to home
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+
